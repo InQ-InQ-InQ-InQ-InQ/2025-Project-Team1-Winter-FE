b829aa25dad135d90fa3eb6321a6ddf968d3331a
cho4u4o <cho4u4o@gmail.com>
2025-02-02 21:32:29 +0900

    remove: delete annotations and unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Prompt } from "next/font/google";
+import { Prompt } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const prompt = Prompt({
   variable: "--font-prompt",
@@ -29,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${prompt.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }

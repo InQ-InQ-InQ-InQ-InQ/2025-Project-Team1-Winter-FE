e17a6108dcd0a72963d5118b3c0858653381752c
cho4u4o <cho4u4o@gmail.com>
2025-02-02 18:54:29 +0900

    feat: implement search button, add tailwind prettier

--- src/widgets/home/ui/HomeSearch.tsx
+++ src/widgets/home/ui/HomeSearch.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import React from "react";
+import { IoSearch } from "react-icons/io5";
 
 export default function HomeSearch() {
   return (
-    <input className="w-[80%] p-2 px-4 sm:w-[60%] focus:outline-hidden border bg-white border-border/80 rounded-lg h-[50px]"></input>
+    <div className="border-border-color/80 relative h-[50px] w-[80%] rounded-lg border bg-white p-2 px-4 sm:w-[60%]">
+      <button
+        className="absolute top-0 right-0 h-full w-fit cursor-pointer rounded-2xl bg-white p-2"
+        onClick={() => alert("아직 만드는중!")}
+      >
+        <IoSearch className="text-2xl text-black/70" />
+      </button>
+      <input className="size-full focus:outline-hidden"></input>
+    </div>
   );
 }

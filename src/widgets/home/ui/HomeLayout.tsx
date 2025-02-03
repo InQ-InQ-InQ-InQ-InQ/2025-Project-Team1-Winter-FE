b743a2ddcb211c627f939d22ab6d86ae5fb19dcf
cho4u4o <cho4u4o@gmail.com>
2025-02-03 22:40:46 +0900

    design: add more space between logo and searchbar

--- src/widgets/home/ui/HomeLayout.tsx
+++ src/widgets/home/ui/HomeLayout.tsx
@@ -8,12 +8,12 @@ import { ProfileButton } from "@/shared/ui";
 
 export default function HomeLayout() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <AnimatedSlider />
       <ProfileButton className="top-16 right-2 md:right-4" />
-      <span className="prompt-extrabold text-6xl">SpotShare</span>
+      <span className="prompt-extrabold mb-10 text-6xl">SpotShare</span>
       <HomeSearch />
-      <span>실시간 인기 포토스팟을 찾아보세요!</span>
+      <span className="mt-2">실시간 인기 포토스팟을 찾아보세요!</span>
       <HomeRegionButtons />
     </div>
   );

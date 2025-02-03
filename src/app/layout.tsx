84a98694bcf430ba7a14e4f5b970194ac3b20bf0
cho4u4o <cho4u4o@gmail.com>
2025-02-03 22:50:25 +0900

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
 import { Prompt } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const prompt = Prompt({
   variable: "--font-prompt",
@@ -10,6 +10,9 @@ const prompt = Prompt({
 export const metadata: Metadata = {
   title: "SpotShare",
   description: "포토스팟 공유 플랫폼",
+  icons: {
+    icon: "/favicon/favicon.ico",
+  },
 };
 
 export default function RootLayout({

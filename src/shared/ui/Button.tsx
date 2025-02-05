488dc0947121b778159e3cd500ad3eddccea92a3
cho4u4o <cho4u4o@gmail.com>
2025-02-05 23:04:12 +0900

    design: change focus color

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button({ name }: { name: string }) {
   return (
-    <button className="hover:bg-primary-200 z-10 w-fit cursor-pointer rounded-lg border bg-white p-2 px-4">
+    <button className="hover:bg-sub z-10 w-fit cursor-pointer rounded-lg border bg-white p-2 px-4 transition delay-100 ease-in-out hover:text-white focus:outline-hidden">
       {name}
     </button>
   );
--- src/shared/ui/TextField.tsx
+++ src/shared/ui/TextField.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function TextField() {
-  return <input className="h-12 w-full rounded-lg border bg-white p-3" />;
+  return (
+    <input className="focus:border-sub th-12 w-full rounded-lg border bg-white p-3 transition delay-100 ease-in-out focus:outline-hidden" />
+  );
 }

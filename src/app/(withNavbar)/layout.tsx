8e3e7380d9abdd142c65caf009c9b0bfaede9fb9
cho4u4o <cho4u4o@gmail.com>
2025-02-09 18:11:39 +0900

    design: fix footer at the bottom

--- src/app/(withNavbar)/layout.tsx
+++ src/app/(withNavbar)/layout.tsx
@@ -6,7 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Nav />
       <div className="flex size-full p-4 pb-0">{children}</div>
-      <footer className="flex-shrink-0 p-4 py-6 text-center text-gray-600">
+      <footer className="bottom-0 mt-auto flex-shrink-0 p-4 py-6 text-center text-gray-600">
         © 2024 SpotShare. All rights reserved.
       </footer>
     </div>

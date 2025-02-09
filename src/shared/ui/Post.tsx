7e33b5f618241ed0cded1a41478361256d28c31c
cho4u4o <cho4u4o@gmail.com>
2025-02-09 17:40:42 +0900

    feat: add button area

--- src/shared/ui/Post.tsx
+++ src/shared/ui/Post.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { IoIosHeartEmpty, IoIosHeart, IoMdShare } from "react-icons/io";
 
 export default function Post() {
   return (
     <div className="flex h-60 flex-col rounded-lg border">
       <div className="bg-bgGray flex-1 rounded-t-lg">photo</div>
-      <div className="h-10 w-full rounded-b-lg bg-white">button</div>
+      <PostButton />
+    </div>
+  );
+}
+
+function PostButton() {
+  const [liked, setLiked] = useState(false);
+  return (
+    <div className="flex w-full justify-between rounded-b-lg bg-white p-2">
+      <div className="flex justify-center gap-x-1">
+        <button
+          className="hover:text-gray cursor-pointer"
+          onClick={() => setLiked((prev) => !prev)}
+        >
+          {liked ? <IoIosHeart color="red" /> : <IoIosHeartEmpty />}
+        </button>
+        <span className="text-sm">1</span>
+      </div>
+      <IoMdShare />
     </div>
   );
 }
